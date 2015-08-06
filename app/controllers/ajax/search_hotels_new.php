5dc320b07086d926a26f518b3a022935c67c6d54
yeminhtut <yehtut.it@gmail.com>
2015-08-06 18:14:46 +0800

    angular view

--- app/controllers/ajax/search_hotels_new.php
+++ app/controllers/ajax/search_hotels_new.php
@@ -1,5 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');  
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 use GuzzleHttp\Client;
 function _search_hotels_new()
 {
